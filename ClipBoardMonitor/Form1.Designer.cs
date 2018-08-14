a874793da9370d6070e8ae5bdad0b3f724f5ce12
TaoYing <barryying>
2018-08-14 18:11:29 +0800

    对文本和图片进行监听

---
+++ ClipBoardMonitor/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ClipBoardMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.清空历史ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(973, 346);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 126);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(973, 489);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(227, 112);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1054, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 53);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(890, 584);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.清空历史ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1226, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 清空历史ToolStripMenuItem
+            // 
+            this.清空历史ToolStripMenuItem.Name = "清空历史ToolStripMenuItem";
+            this.清空历史ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.清空历史ToolStripMenuItem.Text = "清空历史";
+            this.清空历史ToolStripMenuItem.Click += new System.EventHandler(this.清空历史ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(743, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 649);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ClipBoardMonitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 清空历史ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ClipBoardMonitor/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipBoardMonitor
+{
+    public partial class Form1 : Form
+    {
+        #region Definitions
+        //Constants for API Calls...
+        private const int WM_DRAWCLIPBOARD = 0x308;
+        private const int WM_CHANGECBCHAIN = 0x30D;
+
+        //Handle for next clipboard viewer...
+        private IntPtr mNextClipBoardViewerHWnd;
+
+        //API declarations...
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static public extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static public extern bool ChangeClipboardChain(IntPtr HWnd, IntPtr HWndNext);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 添加ListView表头标题
+            this.listView1.Columns.Add("序号", 40, HorizontalAlignment.Left); //添加标题
+            this.listView1.Columns.Add("复制的文本", 600, HorizontalAlignment.Left);
+            this.listView1.View = System.Windows.Forms.View.Details;
+            
+            mNextClipBoardViewerHWnd = SetClipboardViewer(this.Handle);
+        }
+        //重写wndproc方法  定义热键使用
+        #region Message Process
+        //Override WndProc to get messages...
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    {
+                        //The clipboard has changed...
+                        //##########################################################################
+                        // Process Clipboard Here :)........................
+                        //##########################################################################
+                        SendMessage(mNextClipBoardViewerHWnd, m.Msg, m.WParam.ToInt32(), m.LParam.ToInt32());
+
+                        //显示剪贴板中的文本信息
+                        if (Clipboard.ContainsText())
+                        {
+                            AddListView(Clipboard.GetText());
+                            richTextBox1.Text = Clipboard.GetText();
+                        }
+                        //显示剪贴板中的图片信息
+                        if (Clipboard.ContainsImage())
+                        {
+                            pictureBox1.Image = Clipboard.GetImage();
+                            pictureBox1.Update();
+                        }
+                        break;
+                    }
+                case WM_CHANGECBCHAIN:
+                    {
+                        //Another clipboard viewer has removed itself...
+                        if (m.WParam == (IntPtr)mNextClipBoardViewerHWnd)
+                        {
+                            mNextClipBoardViewerHWnd = m.LParam;
+                        }
+                        else
+                        {
+                            SendMessage(mNextClipBoardViewerHWnd, m.Msg, m.WParam.ToInt32(), m.LParam.ToInt32());
+                        }
+                        break;
+                    }
+            }
+            base.WndProc(ref m);
+        }
+        #endregion
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //从观察链中删除本观察窗口  
+            ChangeClipboardChain(this.Handle, mNextClipBoardViewerHWnd);
+
+            //将WM_DRAWCLIPBOARD消息传递到下一个观察链中的窗口   
+            SendMessage(mNextClipBoardViewerHWnd, WM_CHANGECBCHAIN, this.Handle.ToInt32(), mNextClipBoardViewerHWnd.ToInt32());
+        }
+
+        private void AddListView(string value)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                foreach (ListViewItem lt in listView1.Items)
+                {
+                    //MessageBox.Show(Convert.ToString(lt.SubItems[1].Text));
+                    if (lt.SubItems[1].Text.Equals(value))     //比较每一行记录的第2个字段值
+                    {
+                        this.label1.Text = "这项记录已存在！";
+                        return;
+                    }
+                }
+            }
+
+            if (!value.Equals(""))
+            {
+                this.listView1.BeginUpdate(); //数据更新，UI暂时挂起
+
+                // 添加序号
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = (this.listView1.Items.Count + 1).ToString();
+
+                // 添加子项
+                lvi.SubItems.Add(value.TrimStart().TrimEnd());
+                this.listView1.Items.Add(lvi);
+
+                this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制。
+                this.label1.Text = "添加了一项记录！";
+
+                // ListView获取焦点
+                listView1.Focus();
+            }
+        }
+
+        // 测试代码
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mNextClipBoardViewerHWnd = SetClipboardViewer(this.Handle);
+            //// GetDataObject检索当前剪贴板上的数据
+            //IDataObject iData = Clipboard.GetDataObject();
+            //// 将数据与指定的格式进行匹配，返回bool
+            //if (iData.GetDataPresent(DataFormats.Text))
+            //{
+            //    // GetData检索数据并指定一个格式
+            //    this.richTextBox1.Text = (string)iData.GetData(DataFormats.Text);
+            //}
+            //else
+            //{
+            //    MessageBox.Show("剪贴板中数据不可转换为文本", "错误");
+            //}
+        }
+
+        private void 清空历史ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.listView1.Items.Clear();
+            this.label1.Text = "已清空了所有记录！";
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count == 0)
+                return;
+
+            //前提，listview禁止多选
+            ListViewItem currentRow = listView1.SelectedItems[0];
+            Clipboard.SetDataObject(currentRow.SubItems[1].Text,true);
+            this.label1.Text = "已复制到剪贴板！";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("退出后将只保留最后一条复制的内容，是否退出?", "提示:", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+            if (dr == DialogResult.OK)   //如果单击“是”按钮
+            {
+                e.Cancel = false;                 //关闭窗体
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                e.Cancel = true;                  //不执行操作
+            }
+        }
+    }
+}
