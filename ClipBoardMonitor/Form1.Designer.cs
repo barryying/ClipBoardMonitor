ed80263fd158f21bb60aa6d2eb06de5a03bb9aa1
TaoYing <barryying>
2018-08-16 18:18:33 +0800

    1.新增图片文件比对

--- ClipBoardMonitor/Form1.Designer.cs
+++ ClipBoardMonitor/Form1.Designer.cs
@@ -28,36 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.清空历史ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(973, 346);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(227, 126);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(973, 489);
+            this.richTextBox1.Location = new System.Drawing.Point(1193, 365);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(227, 112);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1054, 290);
+            this.button1.Location = new System.Drawing.Point(1274, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -71,7 +67,7 @@
             this.listView1.FullRowSelect = true;
             this.listView1.Location = new System.Drawing.Point(12, 53);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(890, 584);
+            this.listView1.Size = new System.Drawing.Size(660, 550);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
@@ -83,7 +79,7 @@
             this.清空历史ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1226, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1142, 28);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -98,27 +94,63 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Fuchsia;
-            this.label1.Location = new System.Drawing.Point(743, 35);
+            this.label1.Location = new System.Drawing.Point(606, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 15);
             this.label1.TabIndex = 6;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(690, 53);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(440, 550);
+            this.listView2.TabIndex = 7;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.DoubleClick += new System.EventHandler(this.listView2_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(16, 613);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "* 双击当前行可复制文本至剪贴板";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(694, 616);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(278, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "* 双击当前行可打开图片并复制至剪贴板";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1226, 649);
+            this.ClientSize = new System.Drawing.Size(1142, 640);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "ClipBoardMonitor";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -127,13 +159,16 @@
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 清空历史ToolStripMenuItem;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ClipBoardMonitor/Form1.cs
+++ ClipBoardMonitor/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -30,13 +32,22 @@ namespace ClipBoardMonitor
         public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
         #endregion
 
+        private string imageDirectory = Application.StartupPath + "\\Images";
+
         public Form1()
         {
             InitializeComponent();
-
+            if (!Directory.Exists(imageDirectory))
+            {
+                Directory.CreateDirectory(imageDirectory);
+            }
+            else
+            {
+                DelectDir(imageDirectory);
+            }
             // 添加ListView表头标题
             this.listView1.Columns.Add("序号", 40, HorizontalAlignment.Left); //添加标题
-            this.listView1.Columns.Add("复制的文本", 600, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("复制的文本", 400, HorizontalAlignment.Left);
             this.listView1.View = System.Windows.Forms.View.Details;
             
             mNextClipBoardViewerHWnd = SetClipboardViewer(this.Handle);
@@ -59,14 +70,15 @@ namespace ClipBoardMonitor
                         //显示剪贴板中的文本信息
                         if (Clipboard.ContainsText())
                         {
-                            AddListView(Clipboard.GetText());
+                            AddTxtListView(Clipboard.GetText());
                             richTextBox1.Text = Clipboard.GetText();
                         }
                         //显示剪贴板中的图片信息
                         if (Clipboard.ContainsImage())
                         {
-                            pictureBox1.Image = Clipboard.GetImage();
-                            pictureBox1.Update();
+                            AddPictureListView(Clipboard.GetImage());
+                            //pictureBox1.Image = Clipboard.GetImage();
+                            //pictureBox1.Update();
                         }
                         break;
                     }
@@ -97,7 +109,7 @@ namespace ClipBoardMonitor
             SendMessage(mNextClipBoardViewerHWnd, WM_CHANGECBCHAIN, this.Handle.ToInt32(), mNextClipBoardViewerHWnd.ToInt32());
         }
 
-        private void AddListView(string value)
+        private void AddTxtListView(string value)
         {
             if (listView1.Items.Count > 0)
             {
@@ -124,11 +136,66 @@ namespace ClipBoardMonitor
                 lvi.SubItems.Add(value.TrimStart().TrimEnd());
                 this.listView1.Items.Add(lvi);
 
+                this.label1.Text = "添加了一个文本！";
                 this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制。
-                this.label1.Text = "添加了一项记录！";
 
                 // ListView获取焦点
                 listView1.Focus();
+            }
+        }
+
+        private void AddPictureListView(Image image)
+        {
+
+            if (listView2.Items.Count > 0)
+            {
+                foreach (ListViewItem lt in listView2.Items)
+                {
+                    string imagePath = imageDirectory + "\\image" + (lt.ImageIndex + 1).ToString() + ".png";
+                    //Image imageSource = Image.FromFile(imagePath);
+                    SimilarPhoto similarPhotoSource = new SimilarPhoto(imagePath);
+                    string imageSourceHash = similarPhotoSource.GetHash();
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                        SimilarPhoto similarPhotoAdd = new SimilarPhoto(ms);
+                        string imageHash = similarPhotoAdd.GetHash();
+
+                        //MessageBox.Show(Convert.ToString(lt.SubItems[1].Text));
+                        if (SimilarPhoto.CalcSimilarDegree(imageSourceHash, imageHash) <= 5)
+                        {
+                            this.label1.Text = "这项记录已存在！";
+                            ms.Close();
+                            ms.Dispose();
+                            return;
+                        }
+                    }
+                }
+            }
+
+            if (image != null)
+            {
+                //listView2.Items.Clear();
+                //imageList1.Images.Clear();
+                this.listView2.BeginUpdate(); //数据更新，UI暂时挂起
+                image.Save(imageDirectory + "\\image" + (imageList1.Images.Count + 1).ToString() + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                imageList1.ImageSize = new Size(256, 120);
+                imageList1.ColorDepth = ColorDepth.Depth32Bit;
+                this.imageList1.Images.Add(image);
+                //imageList1.Images[imageList1.Images.Count - 1].Tag = imageList1.Images.Count;
+
+                listView2.LargeImageList = imageList1;
+                var lvi = new ListViewItem();
+                lvi.ImageIndex = imageList1.Images.Count - 1;
+                //lvi.Text = apptitlelist[i];//Path.GetFileNameWithoutExtension(filePath);//"P" + i;
+                //lvi.ToolTipText = apppathlist[i];//Path.GetFileNameWithoutExtension(filePath);//"P" + i;
+                listView2.Items.Add(lvi);
+                this.label1.Text = "添加了一张图片！";
+                this.listView2.EndUpdate();  //结束数据处理，UI界面一次性绘制。
+
+                // ListView获取焦点
+                listView2.Focus();
             }
         }
 
@@ -152,10 +219,48 @@ namespace ClipBoardMonitor
 
         private void 清空历史ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.listView1.Items.Clear();
-            this.label1.Text = "已清空了所有记录！";
-        }
+            DialogResult dr = MessageBox.Show("清空后将不保留所有内容(包括本地存放的图片文件)，是否清空?", "提示:", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
 
+            if (dr == DialogResult.OK)   //如果单击“是”按钮
+            {
+                this.listView1.Items.Clear();
+                this.listView2.Items.Clear();
+                this.imageList1.Images.Clear();
+                if (Directory.Exists(imageDirectory))
+                {
+                    DelectDir(imageDirectory);
+                }
+                this.label1.Text = "已清空了所有记录！";
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                this.label1.Text = "您取消了清空历史的操作！";
+            }
+        }
+        public static void DelectDir(string srcPath)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(srcPath);
+                FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();  //返回目录中所有文件和子目录
+                foreach (FileSystemInfo i in fileinfo)
+                {
+                    if (i is DirectoryInfo)            //判断是否文件夹
+                    {
+                        DirectoryInfo subdir = new DirectoryInfo(i.FullName);
+                        subdir.Delete(true);          //删除子目录和文件
+                    }
+                    else
+                    {
+                        File.Delete(i.FullName);      //删除指定文件
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
         private void listView1_DoubleClick(object sender, EventArgs e)
         {
             if (this.listView1.SelectedItems.Count == 0)
@@ -164,6 +269,45 @@ namespace ClipBoardMonitor
             //前提，listview禁止多选
             ListViewItem currentRow = listView1.SelectedItems[0];
             Clipboard.SetDataObject(currentRow.SubItems[1].Text,true);
+            this.label1.Text = "已复制到剪贴板！";
+        }
+
+        private void listView2_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.listView2.SelectedItems.Count == 0)
+                return;
+
+            //前提，listview禁止多选
+            ListViewItem currentRow = listView2.SelectedItems[0];
+            //Image image = currentRow.ImageList.Images[currentRow.ImageIndex];
+
+            string imagePath = imageDirectory + "\\image" + (currentRow.ImageIndex + 1).ToString() + ".png";
+
+            FileStream fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+            Image image = Image.FromStream(fs);
+            fs.Close();
+
+            //建立新的系统进程    
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //设置文件名，此处为图片的真实路径+文件名    
+            process.StartInfo.FileName = imagePath;
+            //此为关键部分。设置进程运行参数，此时为最大化窗口显示图片。    
+            process.StartInfo.Arguments = "rundll32.exe C://WINDOWS//system32//shimgvw.dll,ImageView_Fullscreen";
+            //此项为是否使用Shell执行程序，因系统默认为true，此项也可不设，但若设置必须为true    
+            process.StartInfo.UseShellExecute = true;
+            //此处可以更改进程所打开窗体的显示样式，可以不设    
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            process.Start();
+            process.Dispose();
+
+            //image = pictureProcess(image, 1000, 600);
+            //image = resizeImage(image, new Size(1000, 600));
+            Clipboard.SetImage(image);
+            //Form2 f2 = new Form2();
+            //f2.Form2Value = image;
+            //f2.ShowDialog();
+            //pictureBox1.Image = image;
+            //pictureBox1.Update();
             this.label1.Text = "已复制到剪贴板！";
         }
 
@@ -180,5 +324,59 @@ namespace ClipBoardMonitor
                 e.Cancel = true;                  //不执行操作
             }
         }
+        private static Image resizeImage(Image imgToResize, Size size)
+        {
+            //获取图片宽度
+            int sourceWidth = imgToResize.Width;
+            //获取图片高度
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //计算宽度的缩放比例
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //计算高度的缩放比例
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //期望的宽度
+            int destWidth = (int)(sourceWidth * nPercent);
+            //期望的高度
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //绘制图像
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return b;
+        }
+        public static bool ImageCompareString(Image firstImage, Image secondImage)
+        {
+            MemoryStream ms = new MemoryStream();
+            firstImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            String firstBitmap = Convert.ToBase64String(ms.ToArray());
+            ms.Position = 0;
+
+            secondImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            String secondBitmap = Convert.ToBase64String(ms.ToArray());
+
+            if (firstBitmap.Equals(secondBitmap))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        
     }
 }
